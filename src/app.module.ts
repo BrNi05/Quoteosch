77fb3b22dbaa816e622be9c00e2aa5295d6a5eb1
BrNi05 <szocsbarnabas8@gmail.com>
2025-10-05 01:51:04 +0200

    Add dotenv validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD, APP_FILTER } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuoteModule } from './quote/quote.module';
 import { LecturerModule } from './lecturer/lecturer.module';
@@ -15,7 +16,18 @@ import { ThrottlerModule } from '@nestjs/throttler';
     TypeOrmModule.forRoot(AppDataSource.options),
     QuoteModule,
     LecturerModule,
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().min(32).required(),
+        PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string().valid('development', 'production').default('production'),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
     ThrottlerModule.forRoot({ throttlers: [{ ttl: 60000, limit: 10 }] }),
   ],
   providers: [
